ef957105a00a66065637084726d4a41bc34608ee
abdullah <45887687+heartwhite@users.noreply.github.com>
2019-05-02 01:20:54 +0200

    Update compairingInfinities.js
    I took infinitives into Variables , than compared to make it more readable .

--- Week1/compairingInfinities.js
+++ Week1/compairingInfinities.js
@@ -1,13 +1,19 @@
 // store diffrent types of data in one array, and compare the infinitives
-let allTypesInsideArray = ['abdullah ', 33 , true]
+let allTypesInsideArray = ['abdullah ', 33 , true];
 console.log(allTypesInsideArray);
 
 
 console.log('2 examples for compairing infinities')
-if(6/0 === 10/0){
+let infinitive1 = 6/0;
+let infinitive2 = 10/0;
+if(infinitive1 === infinitive2){
     console.log(true);
 } else{
     console.log(false);
 }
 
-console.log(6/0 == 10/0);
+console.log(infinitive1 == infinitive2);
+
+/*in exercise of infinitives i used == and === both 
+because in this case i wanted to see each option. 
+i guess types are same but i just anted to see */

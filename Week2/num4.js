cf33295aa1d996cb33732448d23c400939ae802c
Abdullah Al Hommada <48802189+aalhommada@users.noreply.github.com>
2019-05-01 22:43:58 +0200

    correct the content of console.log   , and add ';'at the end .

--- Week2/num4.js
+++ Week2/num4.js
@@ -1,12 +1,12 @@
 // 4
 let y = "Abdullah is doing homework";
 // 4.1
-console.log("the value of variabl y will be : Abdullah is doing the homework")
+console.log("the value of variabl y will be : Abdullah is doing homework");
 // 4.2
-console.log(y)
+console.log(y);
 // 4.3 
 y = "I'm doing it perfectly";
 // 4.4
-console.log("the value of variable y will be : I'm doning that perfectly")
+console.log("the value of variable y will be : I'm doning it perfectly");
 // 4.5
-console.log(y)
+console.log(y);

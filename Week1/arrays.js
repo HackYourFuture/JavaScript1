2e3f3c194f944495e4f508eb7d2d754445450ff1
abdullah <45887687+heartwhite@users.noreply.github.com>
2019-05-02 16:12:43 +0200

    Update arrays.js
    .push chanced to myFavoriteAnimals variable

--- Week1/arrays.js
+++ Week1/arrays.js
@@ -4,5 +4,5 @@ console.log ('my array will contain some animals');
 console.log(arraysInside);
 let myFavoriteAnimals = ['eagle ', 'dog ', 'horse '];
 console.log(myFavoriteAnimals);
-arraysInside.push('baby pig');
-console.log(arraysInside);
+myFavoriteAnimals.push('baby pig');
+console.log(myFavoriteAnimals);

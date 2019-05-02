7111dd7990f58486eeba22f77e9a3ada066e636f
Abdullah Al Hommada <48802189+aalhommada@users.noreply.github.com>
2019-05-02 11:37:17 +0200

    add ';' at the end of console.log

--- Week2/num5.js
+++ Week2/num5.js
@@ -1,18 +1,20 @@
 // 5
 console.log(Math.round(7.25));
 // 5.1
-let z = 7.25 ;
+let z = 7.25;
 // 5.2
-console.log(z)
+console.log(z);
 // 5.3
-let a = Math.round(z)
+let a = Math.round(z);
 // 5.4
-console.log(a)
+console.log(a);
 // 5.5 
-let highValue ;
+let highValue;
 if (a > z) {
     highValue = a;
-} else { highValue = z}
+} else {
+    highValue = z
+}
 
 //5.6
 console.log(highValue)

bf3fd0965e606d0879791b62feefbeb59316da7f
abdullah <45887687+heartwhite@users.noreply.github.com>
2019-05-06 15:59:50 +0200

    Update numberRounding.js

--- Week1/numberRounding.js
+++ Week1/numberRounding.js
@@ -4,7 +4,12 @@ console.log(z);
 let a = Math.round(z);
 console.log(a);
 let numbers = [a, z];
-numbers.sort(function(a, b) { return b - a; });
+let highNumber=(Math.max(z,a));
+console.log(highNumber);
 
+
+/* ----this is what i found first---
+numbers.sort(function(a, b) { return b - a; });
 var highest = numbers[0];
-console.log(highest);
+console.log(highest); 
+*/

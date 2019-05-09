58c030f9a95a788ea444bca3aab99651f6cacb68
zehra kopar <48477070+zehrakopar@users.noreply.github.com>
2019-05-09 13:20:58 +0200

    Update file6.js
    done

--- js homework week2/file6.js
+++ js homework week2/file6.js
@@ -1,6 +1,6 @@
-let flowers = [];
-console.log('the value of my array will be: names of flowers');
-console.log(flowers);
+let emptyArray = [];
+console.log('the value of my array will be: empty');
+console.log(emptyArray);
 let myFavoriteAnimals = ['dolphin','butterfly','horse'];
 console.log(myFavoriteAnimals);
 myFavoriteAnimals.push('baby pig');

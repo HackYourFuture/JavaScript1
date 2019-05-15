75510a1f7e1bc2c3a4ceb17e281b6e54585a309f
Riam Alali <48811666+RiamAlali@users.noreply.github.com>
2019-05-15 11:21:55 +0200

    Delete More-JS-1.js

--- Week3/More-JS-1.js
+++
@@ -1,8 +0,0 @@
-
-
-//1 Create a function that takes 3 arguments and returns the sum of the these arguments.
-function sum(a, b, c) {
-    return a + b + c;
-  }
-  let result = sum(1, 2, 3);
-  console.log(result);

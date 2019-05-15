41147aecaaa86cc8ae2df135bd2f722ed6f83d00
veliataseven <44811112+veliataseven@users.noreply.github.com>
2019-05-15 15:00:00 +0200

    Update moreJavaScript-10.js

--- Week3/week2/moreJavaScript-10.js
+++ Week3/week2/moreJavaScript-10.js
@@ -1,9 +1,9 @@
 function advertisement(vehicles) {
     let text = "Amazing Joe's Garage, we service ";
     for(let index = 0; index < (vehicles.length - 2); index++) {
-        text += (vehicles[index] + ", ");
+        text += (vehicles[index] + "s, ");
     }
-    console.log(text + vehicles[vehicles.length -2] + " and " + vehicles[vehicles.length -1] + ".");
+    console.log(text + vehicles[vehicles.length -2] + "s and " + vehicles[vehicles.length -1] + "s.");
 }
 
 advertisement(vehicles);

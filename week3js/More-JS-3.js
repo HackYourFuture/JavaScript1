fce46b21b7db31f8e1823233a80981ed1fa68de5
Riam <riam.alali@outlook.com>
2019-05-15 11:03:59 +0200

    add RiamAlali-week3js

---
+++ week3js/More-JS-3.js
@@ -0,0 +1,22 @@
+
+/*3 Create an object and a function that takes the object as a parameter and
+ prints out all of its properties and values.*/
+let materials= [
+    {
+        week1 : "HTML",
+    },
+    {
+        week2 : "CSS",
+    },
+    {
+        week3 : "git",
+    }
+]
+function course(materials){
+    for(let i=0; i<materials.length;i++){
+        
+    }
+} 
+let i;
+console.log(materials);
+
---
+++ week3js/More-JS-4.js
@@ -0,0 +1,15 @@
+
+//4 Create a function named vehicleType that receives a color, and a code, 1 for car, 2 for motorbike. And prints 'a blue motorbike' for example when called as vehicleType("blue", 2)
+
+function vehicleType(color,code){
+    if (code === 1) 
+        console.log("a " + color + " car")
+    
+    else if
+        (code===2)
+            console.log("a " + color + " motorbike");
+        
+    
+}
+vehicleType("blue",2);
+

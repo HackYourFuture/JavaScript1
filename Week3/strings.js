1dd02ad58c722ffd9d58e2bb7316f3043f1aacc6
lizozo <hamza.erguc@gmail.com>
2019-05-15 23:40:58 +0300

    Revert "added 3 js files as week 3 homework"
    This reverts commit 26dabef43bce26c39798b2576b5491ff9b3fe41f.

--- Week3/strings.js
+++
@@ -1,9 +0,0 @@
-let myString = "hello,this,is,a,difficult,to,read,sentence";
-
-console.log(myString);
-
-console.log(myString.length);
-
-myString = myString.replace(/,/g, ' ');
-
-console.log(myString);

62e1263f53fb16f604c92ddf9356a67ce2993909
Riam Alali <48811666+RiamAlali@users.noreply.github.com>
2019-05-15 11:22:41 +0200

    Delete More-JS-2.js

--- Week3/More-JS-2.js
+++
@@ -1,6 +0,0 @@
-
-//2 Create a function named colorCar that receives a color, and prints out, 'a red car' for example.
-function colorCar(color){
-    return "a " + color + " car";
-}
-console.log(colorCar("red"));

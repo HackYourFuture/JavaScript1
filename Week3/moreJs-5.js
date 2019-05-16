08875187bf73ea4b8665ef3127114aba7d1282c6
abdullah <45887687+heartwhite@users.noreply.github.com>
2019-05-16 14:17:00 +0200

    second console log added
    i just now understand challenge. I added a second console log using '?'

--- Week3/moreJs-5.js
+++ Week3/moreJs-5.js
@@ -7,8 +7,14 @@ if (3 === 3) {
 
 in the example above, (3 === 3) is always true
  thats why console logs "yes" surely.
-if there is sth that i need to see or understand, unfortunately i did not.
+if there is sth that i need to see or understand, unfortunately i did not. >> **last rows added**
 i am looking for feedback...
 */
 
 console.log("yes");
+
+// ** last rows
+
+console.log ( (3 == 3) ? 'yes' : 'no');
+
+// :))) i just now understand challange. is ti true?

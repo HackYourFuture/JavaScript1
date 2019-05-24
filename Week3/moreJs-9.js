1654040591487b59e3ae1afe0ef82638bf900af6
abdullah <45887687+heartwhite@users.noreply.github.com>
2019-05-24 13:27:11 +0200

    Update moreJs-9.js
    1 - condition's (function) parameter changed as 'age' from 'years'. this way more consistent with lines above.
    2- if statement added for the value of 'type' parameter. console logs the vehicle only if it has vehicle type. otherwise logs an warning

--- Week3/moreJs-9.js
+++ Week3/moreJs-9.js
@@ -3,18 +3,22 @@ Change the function vehicle to use the list of question 7.
  So that vehicle("green", 3, 1) prints "a green new bike".
 */
 
-function vehicle (color, type, age){
-    let vehicles = ['motorbike' , 'caravan', 'bike', 'car'];
-    // variable 'vehicles' is can be read from  moreJs-7and8.js
-    // but i used it again inside of function to show you,
-    // and in case you can run in a html seperately.
-    console.log('a ' + color +' ' + condition(age) + vehicles[(type-1)]);
+function vehicle(color, type, age) {
+  let vehicles = ['motorbike', 'caravan', 'bike', 'car'];
+  // variable 'vehicles' is can be read from  moreJs-7and8.js
+  // but i used it again inside of function to show you,
+  // and in case you can run in a html seperately.
+  if (type > 0 && type <= vehicles.length) {
+    console.log('a ' + color + ' ' + condition(age) + vehicles[type - 1]);
+  } else {
+    console.log('!NO SUCH VEHICLE HERE');
+  }
 }
-function condition(years){
-    if (years<2){
-        return 'new ';
-    } else {
-        return 'used ';
-    }
+function condition(age) {
+  if (age < 2) {
+    return 'new ';
+  } else {
+    return 'used ';
+  }
 }
-vehicle('green' , 3, 1);
+vehicle('green', 5, 1);

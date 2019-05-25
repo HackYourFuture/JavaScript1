cc297b4fab6af6b473fa61b809c99f090bd6853d
Shvan <48798783+Shvan1@users.noreply.github.com>
2019-05-25 22:42:48 +0200

    Update app.js
    I have split the questions and corrected some mistakes that figured out by Marciano.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//1.
 let englishGreeting = "Hello, World!"
 console.log(englishGreeting);
 let kurdishGreeting = "Silav, Cihan!"
@@ -5,65 +6,122 @@ console.log(kurdishGreeting);
 let dutchGreeting = "Hoi, Wereld!"
 console.log(dutchGreeting);
 
+//2.
 let me = "I'm awesome!"
 console.log(me);
 
+//3.1
 let x;
-console.log("I think the value of x is a number");
-console.log("The value of x is 6");
-let x = 6;
-console.log("I think the value of x is 6");
+
+//3.2
+console.log("I think the value of x will be: undefined");
+
+//3.3
 console.log(x);
 
+//3.4
+let x = 6;
+
+//3.5
+console.log("the value of x is: 6");
+
+//3.6
+console.log(x);
+
+//4.1
 let y ="Sivan";
+
+//4.2
 console.log("I think the value of y is a name");
-console.log(y);
-const y = "Hassam";
-console.log("There will be an error because now I have
-             the same variable with different values.");
+
+//4.3
 console.log(y);
 
+//4.4
+const y = "Hassam";
+
+//4.5
+console.log("There will be an error because now I have the same variable with different values.");
+
+//4.6
+console.log(y);
+
+//5.
 const z = 7.25;
+
+//5.1
 console.log(z);
+
+//5.2
 const a = z;
+
+//5.3
 console.log(a);
+
+//5.4
 var round =Math.round(7.25);
+
+//5.6
 console.log(round)
 
-let arrays =
-console.log("I think variable arrays is related to animals");
+//6.1
+let arrays;
+
+//6.2
+console.log("I think the value will be undefined");
+
+//6.3
 console.log(arrays);
+
+//6.4
 let myFavoriteAnimals = [cat, dog, sheep];
+
+//6.5
 console.log(myFavoriteAnimals);
+
+//6.6
 let favoriteAnimalOfDaan = "baby pig";
-const animals = "myFavoriteAnimals" + "" + "favoriteAnimalOfDaan";
+const animals = myFavoriteAnimals + "" + favoriteAnimalOfDaan;
+
+//6.7
 console.log(animals);
 
+//7.1
 let myString = "this is a test";
-console.log(myString);
-console.log(myString ["length"]);
 
+//7.2
+console.log(myString);
+
+//7.3
+console.log(myString.length);
+
+//8.1
 let num = 11;
 let address = "Huygenhoekring";
 let supermarkets = ["Lidl", "Dekka", "Jumbo"];
 let booleans = true, false, true;
+
+//8.2
 console.log(num);
 console.log(address);
 console.log(supermarkets);
 console.log(booleans);
-console.log(I think the type of num is number,
-            type of address is string,  
-            type of supermarkets is array and
-            type of booleans is boolean.);
+
+//8.3
+console.log("I think the type of num is number, type of address is string, type of supermarkets is array and type of booleans is boolean.");
+
+//8.4
 console.log(typeof num);
 console.log(typeof supermarkets);
 console.log(typeof booleans);
 console.log(typeof address);
 
+//8.5
 if (num != address) {
     console.log("num is a number and address is a string");
 }
 
+//9.1
 let s = 7;
 let v = 3;
 let h = s % 3;
@@ -71,10 +129,14 @@ console.log(s % h);
 console.log(s % v);
 console.log(v % h);
 
+//10.1
 const mix = [5, "Shvan"];
 console.log(mix);
 
+//10.2
 var maxNumber = Math.pow(6/0);
 
-if (maxNumber === Infinity)
+//10.2
+if (maxNumber === Infinity){
 console.log(0 / maxNumber);
+}

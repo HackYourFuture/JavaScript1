43101ded54b8aa6847337c9f4d6587e9f772e399
Albawab <32767497+Albawab@users.noreply.github.com>
2019-05-26 23:05:39 +0200

    Update function.js

--- function.js
+++ function.js
@@ -100,18 +100,25 @@ console.log(z==y);//true
 console.log(z==x);//false
 
 // 16
-
-let o1 = { foo: "bar" };
-let o2 = { foo: "bar" };
+//Does the order that you assign (o3 = o2 or o2 = o3) matter? Yes
+let o1 = { foo: 'bar' };
+let o2 = { foo: 'bar' };
 let o3 = o2;
+//Show that changing o2 changes o3 (or not)
+o2.foo = 'par';
+console.log(o3); // Yes it changes
 
-//Show that changing o2 changes o3 (or not) // YES changes
-//changing o1 changes o3(or not).// NO changes
-// Does the order that you assign (o3 = o2 or o2 = o3) matter? It is matter
+console.log(o3);
+o1.foo = 'parw';
+console.log(o3); // No
 
+console.log(o2);
+console.log(o3);
+o2 = o3;
+console.log(o2);
+console.log(o3);
 
-
-// 17
 let bar = 42;
 typeof typeof bar;
- // It is String first i have call the type of the value . second the type of the value was number and the word number is string
+console.log(typeof typeof bar);
+// It is String first i have call the type of the value . second the type of the value was number and the word number is string

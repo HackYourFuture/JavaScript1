7192e2f85aa0be53917baf1fcb23e82dab9bee7c
Oguzhan <oguzhanturgut@outlook.com>
2019-06-14 23:23:21 +0200

    Add isInfinity()

--- Week2/10_various.js
+++ Week2/10_various.js
@@ -1,4 +1,4 @@
-let mixArray = [
+const mixArray = [
   [1, 2, 3],
   ["one", "two", "three", "four"],
   "I am just a string",
@@ -11,10 +11,18 @@ let mixArray = [
   }
 ];
 
+console.table(mixArray); // table preview for Arrays and Objects
+
 console.log(mixArray);
 
 console.log((6 / 0 === 10 / 0)); //true
 console.log(typeof (6 / 0)); //still number
-console.log(6 / 0); //Infinity
+
+// function expression to check if the argument NaN, positive infinity, or negative infinity
+const isInfinity = a => !isFinite(a);
+
+console.log(isInfinity(6 / 0)); //true
+
 console.log(1 / Infinity); //Zero
+
 console.log(Math.round(Infinity)); // Still Infinity :)

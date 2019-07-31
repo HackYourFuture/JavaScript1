5c6f4d8bca65dcaa508aef21509d80ed96d7fa45
Husenjan Obul <48457901+husenjanobul@users.noreply.github.com>
2019-07-31 22:29:02 +0200

    Update arrays.js

--- Week2/arrays.js
+++ Week2/arrays.js
@@ -1,4 +1,5 @@
 var myArrys = [];
+console.log("i think the value of myArrys is : length 0 , []  .");
 console.log("arrays are used to store multiple values in a single variable.");
 console.log(myArrys);
 

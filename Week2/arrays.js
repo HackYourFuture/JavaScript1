f15a040c2d31c671a63676874cd0c59a6c8074d3
Husenjan Obul <48457901+husenjanobul@users.noreply.github.com>
2019-07-31 22:34:57 +0200

    Update arrays.js

--- Week2/arrays.js
+++ Week2/arrays.js
@@ -1,9 +1,9 @@
-var myArrys = [];
+let myArrys = [];
 console.log("i think the value of myArrys is : length 0 , []  .");
 console.log("arrays are used to store multiple values in a single variable.");
 console.log(myArrys);
 
-var petsWorld = ["cat", "dog", "chicken", "fish" ];
+let petsWorld = ["cat", "dog", "chicken", "fish" ];
 console.log(petsWorld);
 petsWorld.push("horse");
 console.log(petsWorld);

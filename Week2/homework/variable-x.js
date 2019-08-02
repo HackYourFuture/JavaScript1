91fdd36d2fed5e7d5e150ce25745f69d06b195dd
lamaajaj <47869620+lamaajaj@users.noreply.github.com>
2019-08-02 11:50:17 +0200

    Update variable-x.js

--- Week2/homework/variable-x.js
+++ Week2/homework/variable-x.js
@@ -3,7 +3,7 @@
 let x;
 
 //3.2 Add a console.log statement that explains in words what you think the value of x is, like in this example:
-console.log("the value of my variable x will be: 25");
+console.log("the value of my variable x will be: undefined");
 
 //3.3 Add a console.log statement that logs the value of x.
 console.log(x)

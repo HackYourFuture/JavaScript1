da1b4f2ce9c6be5a18d7d2c3201c47289f3e1107
es <esrazengn00@gmail.com>
2019-08-05 21:59:00 +0200

    updating helloWorld.js

--- Week2/helloWorld.js
+++ Week2/helloWorld.js
@@ -1,6 +1,6 @@
 console.log('------ 1 ------');
-console.log('Hello World');
-console.log('Merhaba Dunya');
-console.log('Hallo Wereld');
-console.log('こんにちは世界');
+console.log('Hello World');     // English
+console.log('Merhaba Dunya');   // Turkish
+console.log('Hallo Wereld');    // Dutch
+console.log('こんにちは世界');    // Japanese
 console.log('\n');
--- Week2/moreStrings.js
+++ Week2/moreStrings.js
@@ -1,8 +1,8 @@
 console.log(`------ 7 ------`);
 
-let myString = `this is a test`;
+const myString = `this is a test`;
 console.log(myString);
 
-let lengthOfMyString = myString.length;
+const lengthOfMyString = myString.length;
 console.log(`The lenght of my string is: ${lengthOfMyString}`);
 console.log(`\n`);

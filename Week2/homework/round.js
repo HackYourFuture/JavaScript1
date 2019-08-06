d188514a7c6b1f550e0707d5b339f36e916b4941
lamaajaj <47869620+lamaajaj@users.noreply.github.com>
2019-08-06 08:53:00 +0200

    Update round.js

--- Week2/homework/round.js
+++ Week2/homework/round.js
@@ -20,7 +20,7 @@ console.log(a);
 
 
 //5.5 So now we have z and a find a way to compare the two values and store the highest of the two in a new variable.
-let max = Math.max(a, z);
+const max = Math.max(a, z);
 
 
 

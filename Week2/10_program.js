3f7b0f96faa0657b9d630d1bf649d7d11e8117fb
mosleh <mosleh.alnaqeeb@gmail.com>
2019-08-07 23:12:30 +0200

    implementation of feedbacks

--- Week2/10_program.js
+++ Week2/10_program.js
@@ -1,4 +1,17 @@
-let myArray =["My age","30"]; // With array is possibale to store different data, within a single variable
+// 10.1 Can you store multiple types in an array? Numbers and strings? Make an example that illustrates your answer.
+
+let myArray =["My age",30 , ["Student" ,5]]; // With array is possibale to store different data, within a single variable
 console.log(myArray);
-let maxNumber = Math.pow(10, 1000);
-console.log(maxNumber);
+
+
+//10.2 Can you compare infinities? (Not in Eyad's world) - does 6/0 === 10/0? How can you test this?
+//10.3 Add console.log statements to the above program
+let inf1 = 10/0;
+let inf2 = 6/0;
+
+function comparInfinities(inf1,inf2){
+        if (inf2 = Number.MAX_VALUE , inf2 = Number.MAX_VALUE)
+        console.log("inf1 and inf2 are infinity")
+        else console.log("False")
+}
+comparInfinities()
--- Week2/8_checkTypeOfData.js
+++ Week2/8_checkTypeOfData.js
@@ -1,7 +1,12 @@
+// 8.1 First declare at least four variables and assign them different data types
 let num = 5;
 let str = "name";
 let arr = ["blue", "black", "Red"];
 let boo = false;
+
+// 8.2 For each variable write a console.log statement that logs the value
+// 8.3 Now write a console.log statement wherein you first explain in words what you think the type of your variables is.
+// 8.4 Now use typeof to log the actual type of your variables.
 console.log("The value of my variable 'num' is: " + num);
 console.log("num is: Number");
 console.log("The value of my variable 'str' is: " + str);
@@ -11,19 +16,16 @@ console.log("arr is: Array");
 console.log("The value of my variable 'boo' is: " + boo);
 console.log("boo is: Boolean");
 
-function compareType() {
-    if (typeof num == typeof str)
-        console.log("'num' and 'str' are same type")
-    else if (typeof num == typeof arr)
-        console.log("'num' and 'arr' are same type")
-    else if ( typeof num == typeof boo)
-        console.log("'num' and 'boo' are same type")
-    else if ( typeof str == typeof arr)
-        console.log("'str' and 'arr' are same type")
-    else if ( typeof str == typeof boo)
-        console.log("'str' and 'boo' are same type")
-    else if ( typeof arr == typeof boo)
-        console.log("'arr' and 'boo' are same type")
-   else console.log("The variables have been assigned to a different types of values")
-}
-compareType()
+//8.5 Now compare the types of your different variables with one another.
+//8.6 Make sure to also show a message when the variables you are comparing are not the same type.
+function compareType(var1,var2) {
+    if (typeof var1 == typeof var2)
+        console.log( "this variable have the same type")
+        else console.log("The first variable assigned to %o and the second variable assigned to %o ",typeof var1 , typeof var2 )
+    }
+compareType(num,str);
+compareType(num,arr);
+compareType(num,boo);
+compareType(str,arr);
+compareType(str,boo);
+compareType(arr,boo);

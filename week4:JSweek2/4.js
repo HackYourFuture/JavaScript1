da7c7a4bd3f40eed2efd34ce406a0dc53a7fdc45
veyselindi <52565479+veyselindi@users.noreply.github.com>
2019-08-25 17:20:49 +0200

    deleted unnecessary parts6

--- week4:JSweek2/4.js
+++
@@ -1,6 +0,0 @@
-let y = 'isJSreallyHard?';
-console.log("the value of my string will be: isJSreallyHard?");
-console.log(y);
-y='ItLooksLikeThat';
-console.log("the value of my string will be: ItLooksLikeThat");
-console.log(y);

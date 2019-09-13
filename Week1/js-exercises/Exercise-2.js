c298c670429e8c67384e94277bf0d83768475ae4
ozcansamed <ozcansamed61@gmail.com>
2019-09-13 23:59:14 +0200

    Add Exercise-2

---
+++ Week1/js-exercises/Exercise-2.js
@@ -0,0 +1,12 @@
+//  ===EXERCISE 2===
+
+console.log('EXERCISE 2');
+/* In this situation, if we write:
+console.log('I'm awesome');
+it doesn't work and send us an error message.
+In JavaScript, we can escape a quote from considering it as an end of string quote,
+by placing a backslash (\) in front of the quote. */
+console.log("I'm awesome");
+/* Now it works, because of our escaping character, backslash(\) */
+//Or we can change the quotes:
+console.log("I'm awesome");

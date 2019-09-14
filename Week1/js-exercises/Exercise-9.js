ebc9b2e27565cf67699bde33cc86978b3a66e015
ozcansamed <ozcansamed61@gmail.com>
2019-09-14 09:36:09 +0200

    add exercise 5

--- Week1/js-exercises/Exercise-9.js
+++ Week1/js-exercises/Exercise-9.js
@@ -3,3 +3,18 @@
 'use strict';
 
 console.log('EXERCISE 9');
+
+let x = 7;
+x = x % 3;
+console.log('The value of x would be 1 after the calculation');
+console log(x);
+
+let y = 21;
+y = y % 4;
+console.log('The value of y would be 1 after the calculation');
+console log(y);
+
+let z = 13;
+z = z % 2;
+console.log('The value of z would be 1 after the calculation');
+console log(z);

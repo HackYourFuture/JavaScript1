417690489975d44e5c6490471f442e27cfe0ad7b
MA-Hussein <54192398+MA-Hussein@users.noreply.github.com>
2019-09-24 20:28:41 +0200

    Update 6-array-of-animals.js

--- Week1/Js-exercises/6-array-of-animals.js
+++ Week1/Js-exercises/6-array-of-animals.js
@@ -1,8 +1,8 @@
 "use strict";
-let animalsArr;
+const animalsArr;
 console.log("I think the value will be undefined");
 console.log(animalsArr);
-let favoriteAnimals = ["Hamsters", "Cats", "Dogs",];
+const favoriteAnimals = ["Hamsters", "Cats", "Dogs",];
 console.log(favoriteAnimals);
 favoriteAnimals.push("Horses");
 console.log(favoriteAnimals);

e5eeba5db262e67df04edd63ab058abd5e931f30
MA-Hussein <54192398+MA-Hussein@users.noreply.github.com>
2019-09-24 20:42:22 +0200

    Update 9-log-the-remainder.js

--- Week1/Js-exercises/9-log-the-remainder.js
+++ Week1/Js-exercises/9-log-the-remainder.js
@@ -1,11 +1,11 @@
 
 let x = 7;
-x = x % 3 ; // new value of x will be 1 as it's remainder when it's divided by three is 1.
+x = x % 3 ; // the new value of x will be 1 as it's remainder when it's divided by three is 1.
 console.log(x); // we will see it is 1.
 
-let y = 21; // Their value would be the same.Because we didn't assign a new value to them. 
-let z = 13;
+let y = 21; // the new value of y will be 1 . 
+let z = 13; // the new value of z will be 1 . 
 
-console.log(y);  /* We can see they still have their initial values.
-                  Because the remainder includes x but not y and z.*/
+console.log(y); 
+                  
 console.log(z);

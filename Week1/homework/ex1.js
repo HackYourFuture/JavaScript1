1506eda2cb4eabbc77772c861b9211c65f835ae5
Ammar <zarosho@gmail.com>
2019-11-26 11:30:10 +0100

    finished the first one

---
+++ Week1/homework/ex1.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const slovenianGreeting ="Pozdravljen, svet!";
+const englishGreeting = "Hallo World!";
+const dutchGreeting = "Dag Wereld!";
+const arabicGreeting = "Murhaba Ya 3llem!";
+const spanishGreeting ="¡Hola Mundo!";
+const polishGreeting ="Witaj świecie!";
+const portugueseGreeting ="lá Mundo!";
+const hebrewGreeting ="שלום עולם!";
+const italianGreeting ="Ciao mondo!";
+const chineseGreeting ="Nǐ hǎo, shìjiè";
+const swahiliGreeting ="Salamu, Dunia!";
+
+
+console.log(englishGreeting)
+console.log(dutchGreeting)
+console.log(arabicGreeting)
+console.log(slovenianGreeting)
+console.log(spanishGreeting)
+console.log(polishGreeting)
+console.log(hebrewGreeting)
+console.log(italianGreeting)
+console.log(chineseGreeting)
+console.log(swahiliGreeting)
+
+

1a5e0f7fd47cb6aef1a85f55a13af91be3db9e6b
Abuzer ALACA <aalaca.33@gmail.com>
2019-11-26 18:59:50 +0100

    Added logNumber js file

---
+++ Week1/Homework/js-exercises/logNumber.js
@@ -0,0 +1,15 @@
+"usestrict";
+
+var numberX;
+
+console.log(
+  "The numberX will be undefined because we didn't add any value to numberX."
+);
+console.log(numberX);
+
+numberX = 10;
+
+console.log(
+  "The numberX will be an integer which is 10 because I assigned 10 to numberX."
+);
+console.log(numberX);

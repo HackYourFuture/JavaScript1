29cce896a89e441a0a7f55c025dc68c3e6bb10e3
Ammar <zarosho@gmail.com>
2019-11-26 11:49:40 +0100

    finished number four

---
+++ Week1/homework/ex4.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let myString ='Ammar Alsasa';
+
+console.log('My string has my name, isn\'t it cool?');
+console.log(myString);
+
+myString ='Tiny Rick!!!!!';
+
+console.log('My string has Rick\'s name now, isn\'t it cool?');
+console.log(myString);

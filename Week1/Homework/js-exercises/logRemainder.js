5d99ebab72f75d5dd1fcc3046022b86d970136f7
Abuzer ALACA <aalaca.33@gmail.com>
2019-11-26 19:40:16 +0100

    Updated logRemainder js file

--- Week1/Homework/js-exercises/logRemainder.js
+++ Week1/Homework/js-exercises/logRemainder.js
@@ -1,16 +1,22 @@
 "use strict";
 
 var x = 7;
+console.log("The value of x is ", x);
 
-// The result will be 2. % is the operator to get the remainder. When we divide 7 by 5, we get 1 as quotient and 2 as remainder.
-x = x % 5;
+// The result will be 1. % is the operator to get the remainder. When we divide 7 by 3, we get 2 as quotient and 1 as remainder.
+x = x % 3;
+console.log("The value of x after taking modulus is: ", x);
 
 var y = 21;
+console.log("The value of y is ", y);
 
 // The result will be 1. % is the operator to get the remainder. When we divide 21 by 4, we get 5 as quotient and 1 as remainder.
 y = y % 4;
+console.log("The value of x after taking modulus is: ", y);
 
 var z = 13;
+console.log("The value of z is ", z);
 
 // The result will be 1. % is the operator to get the remainder. When we divide 13 by 2, we get 6 as quotient and 1 as remainder.
 z = z % 2;
+console.log("The value of x after taking modulus is: ", z);

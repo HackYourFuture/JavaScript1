7122ca8f8f660d5d79a84517a9ff5965ce618f2d
Abuzer ALACA <aalaca.33@gmail.com>
2019-11-26 18:50:06 +0100

    Added first and second homework

---
+++ Week1/Homework/js-exercises/logHello.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log("Merhaba, Dunya");
+console.log("Hello, World");
+console.log("Cześć, świecie");
+console.log("Ola mundo");
+console.log("Привет Дуня");
+console.log("नमस्कार, दुन्या");
+console.log("こんにちは、World");
+console.log("Silav, cîhan");
+console.log("مرحبا ، العالم");
+console.log("Hola mundo");

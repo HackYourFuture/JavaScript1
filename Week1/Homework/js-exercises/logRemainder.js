886474c906755f516f17439a0c82544faee06100
Abuzer ALACA <aalaca.33@gmail.com>
2019-11-26 19:34:48 +0100

    Added logRemainder js file

---
+++ Week1/Homework/js-exercises/logRemainder.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var x = 7;
+
+// The result will be 2. % is the operator to get the remainder. When we divide 7 by 5, we get 1 as quotient and 2 as remainder.
+x = x % 5;
+
+var y = 21;
+
+// The result will be 1. % is the operator to get the remainder. When we divide 21 by 4, we get 5 as quotient and 1 as remainder.
+y = y % 4;
+
+var z = 13;
+
+// The result will be 1. % is the operator to get the remainder. When we divide 13 by 2, we get 6 as quotient and 1 as remainder.
+z = z % 2;

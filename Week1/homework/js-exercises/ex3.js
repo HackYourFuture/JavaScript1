57cc16eb3a58602e6092c1e2b2f859d937c6b801
Dima <dimakaddah@gmail.com>
2019-11-26 23:10:48 +0100

    add exercise3

---
+++ Week1/homework/js-exercises/ex3.js
@@ -0,0 +1,14 @@
+//Exercise 3: Log the number
+//declare your variable numberX
+let numberX;
+//what i think the value of x is
+console.log('value of numberX undefined');
+//log numberX
+console.log(numberX);
+//initialize your variable with number value
+numberX = 30;
+//what you think the value of numberX is
+console.log('the value of numberX is number');
+//log value and type of numberX
+console.log(numberX);
+console.log(typeof numberX);

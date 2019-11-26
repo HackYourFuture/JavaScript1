5033ad9c345c99ace20ddb5af39ec363b3f3170c
Dima <dimakaddah@gmail.com>
2019-11-27 00:33:07 +0100

    add exercise6

---
+++ Week1/homework/js-exercises/ex6.js
@@ -0,0 +1,17 @@
+//Exercise 6: Log an array of animals
+//Declare variable and assign to it an empty array
+const empty = [, , ,];
+// what i think the value of the array is.
+console.log('the arry have empty items, and the type of empty is object');
+//logs the array
+console.log(empty);
+//Create a new variable with an array animal
+const animal = ['cats', 'Dogs', 'Horses'];
+//logs the second array
+console.log(animal[1]);
+//adds another string to the arry
+animal.unshift('Piglet');
+//logs the second array
+console.log(animal[1]);
+//log the arry
+console.log(animal);

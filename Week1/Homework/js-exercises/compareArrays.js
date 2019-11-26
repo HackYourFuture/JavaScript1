b5db470b749b33d46d6ff0e9f2760a695b62e9b0
Abuzer ALACA <aalaca.33@gmail.com>
2019-11-26 19:49:09 +0100

    Added compareArrays js file

---
+++ Week1/Homework/js-exercises/compareArrays.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var me = ["Abuzer", 29, "unemployed", "bachelor graduate"];
+
+var tesla = [
+  "model S",
+  "model X",
+  "model Y",
+  "model 3",
+  "Roadster",
+  "Cyber Truck",
+  "Solar Panel"
+];
+
+console.log("length of the array me is ", me.length);
+console.log("length of the array tesla is ", tesla.length);
+
+if (me.length == tesla.length) {
+  console.log("They are the same!");
+} else {
+  console.log("Two different sizes");
+}

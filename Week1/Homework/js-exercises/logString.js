c1674c4a4a70847ff4e0bf07146dc20088943c26
Abuzer ALACA <aalaca.33@gmail.com>
2019-11-26 19:06:20 +0100

    Adde logString js file

---
+++ Week1/Homework/js-exercises/logString.js
@@ -0,0 +1,14 @@
+"usestrict";
+
+var myString = "Abuzer ALACA";
+console.log(
+  "The value of myString will be my full name because I assigned my name to it."
+);
+console.log(myString);
+
+myString = "There is an apple.";
+
+console.log(
+  "The value of myString will be a sentence which is 'There is an apple.' because I assigned my name to it."
+);
+console.log(myString);

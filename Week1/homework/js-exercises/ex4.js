d57973b47ed23285f652239f43f246893c63fa5f
Dima <dimakaddah@gmail.com>
2019-11-26 23:22:21 +0100

    add exercise4

---
+++ Week1/homework/js-exercises/ex4.js
@@ -0,0 +1,10 @@
+//Exercise 4: Log the string
+//Declare a variable
+var myString = 'Dima' + ' ' + 'Kaddah';
+//what i think the value of the string is ang log it
+console.log(
+  `the value of the string is ${myString}` +
+    ' ' +
+    'Dima Kaddah one time with space between',
+);
+console.log(myString);

864550f92e64d6226caa78ff5243b50c28cc6a50
Dima <dimakaddah@gmail.com>
2019-11-27 00:50:41 +0100

    add exercise9

---
+++ Week1/homework/js-exercises/ex9.js
@@ -0,0 +1,13 @@
+//Exercise 9: Log the remainder
+//x after the calculation
+let x = 7;
+let calculationX = x % 3;
+console.log(`this is the remainder from 7/3 ➽ ${calculationX}(left over)`);
+//y after the calculation
+let y = 21;
+let calculationY = y % 4;
+console.log(`this is the remainder from 21/4 ➽ ${calculationY}(left over)`);
+//z after the calculation
+let Z = 13;
+let calculationZ = Z % 2;
+console.log(`this is the remainder from 13/2 ➽ ${calculationZ}(left over)`);

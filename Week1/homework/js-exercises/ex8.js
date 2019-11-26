c1899a273e1c119488a8f3915a64d78d1d41dd7b
Dima <dimakaddah@gmail.com>
2019-11-27 00:44:21 +0100

    add exercise8

---
+++ Week1/homework/js-exercises/ex8.js
@@ -0,0 +1,25 @@
+//Exercise 8: Type checker
+// Declare all variables
+let first = '';
+let second = '';
+let third = [,];
+let forth = [,];
+
+//Create 6 conditional statements
+typeof first == typeof second; //true
+typeof first !== typeof second; //false
+typeof first == typeof third; //false
+typeof first !== typeof third; //true
+typeof first == typeof forth; //false
+typeof first !== typeof forth; //true
+
+// Check data type
+console.log(typeof first);
+console.log(typeof third);
+
+// Check if data type is the same
+if (typeof first == typeof third) {
+  console.log('SAME TYPE');
+} else {
+  console.log('not the same');
+}

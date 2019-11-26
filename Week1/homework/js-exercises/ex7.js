4d2faf98db442de5291c644dac37b90130172ce5
Dima <dimakaddah@gmail.com>
2019-11-27 00:38:35 +0100

    add exercise7

---
+++ Week1/homework/js-exercises/ex7.js
@@ -0,0 +1,7 @@
+//Exercise 7: Log the length of a string
+//eclare a variable
+let mySentence = 'Programming is so interesting!';
+//get the length of
+let MySentenceLength = mySentence.length;
+//console the length
+console.log(MySentenceLength);

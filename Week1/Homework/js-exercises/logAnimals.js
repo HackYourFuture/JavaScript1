9c668bee2003f6be028463637a3826838b655809
Abuzer ALACA <aalaca.33@gmail.com>
2019-11-26 19:17:36 +0100

    Added logAnimals js file

---
+++ Week1/Homework/js-exercises/logAnimals.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var emptyArrays = [];
+
+console.log("It is an empty array.");
+console.log(emptyArrays);
+
+var animals = ["Dog", "Cat", "Lion"];
+console.log(animals);
+
+animals.push("Piglet");
+console.log(animals);

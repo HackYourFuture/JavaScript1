3748957b0103540a3f23ba12ea604ff00ad176ce
Dima <dimakaddah@gmail.com>
2019-11-27 01:06:15 +0100

    add exercise10

---
+++ Week1/homework/js-exercises/ex10.js
@@ -0,0 +1,13 @@
+//Exercise 10: Compare arrays
+//Declare 2 variables hold an array
+const arry1 = ['one', 'two', 'three', 'four', 'five'];
+const arry2 = ['one', 'two', 'three', 'four', 'five', 'seven', 'eight'];
+//Find out how to get the length of each array
+console.log(arry1.length);
+console.log(arry2.length);
+//Write a conditional statement
+if (arry1.length == arry2.length) {
+  console.log('They are the same!');
+} else {
+  console.log('Two different sizes');
+}

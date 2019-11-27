1843acd15655700f8a57ca3aefc07a84faf80d8b
Ammar <zarosho@gmail.com>
2019-11-27 21:17:56 +0100

    finished ex9

---
+++ Week1/homework/ex9.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//if x equals 7, and the only other statement is x = x % 3, what would be the value of x after the calculation?
+
+//x is 1 
+
+let x = 7;
+x %= 3 ;
+console.log(x);
+
+//If y equals 21, and the only other statement is y = y % 4, what would be the value of y after the calculation?
+
+//y is 1
+
+let y = 21;
+y %= 4; 
+
+console.log(y);
+
+//If z equals 13, and the only other statement is z = z % 2, what would be the value of z after the calculation?
+
+//z is 1
+
+let z = 13;
+z %= 2;
+console.log(z);

00573412a68247477cdd3b9101582dfd010394fd
Ammar <zarosho@gmail.com>
2019-11-27 20:30:10 +0100

    finished ex6

---
+++ Week1/homework/ex6.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let foods =[];
+
+console.log("The array has names of food");
+console.log(foods);
+
+let myAnimals = ['cats', 'dogs', 'horses'];
+
+console.log(myAnimals);
+
+myAnimals.push('Piglet');
+
+console.log(myAnimals);
+
+

c6b154c9ac5de78db7c3110f199b6d7e2db5b6b4
Ramzi <48523685+ramziash@users.noreply.github.com>
2019-11-28 15:00:04 +0100

    Update e5-round a number.js

--- Week1/homework/js-excercises/e5-round a number.js
+++ Week1/homework/js-excercises/e5-round a number.js
@@ -6,9 +6,18 @@ console.log(z);
 let a= Math.floor(z);
 console.log(a);
 
-let highest=[]
-if (z > a) {
-    return  console.log(highest=z);
-} else{
-    return  console.log(highest=a)
-};
+// let highest=[]
+// if (z > a) {
+//     return  console.log(highest=z);
+// } else{
+//     return  console.log(highest=a)
+// };
+
+let highest;
+if (z>a) {
+    highest=z
+}else {
+    highest=a
+}
+
+console.log(highest)

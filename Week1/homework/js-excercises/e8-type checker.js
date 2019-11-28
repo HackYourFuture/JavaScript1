ee354d1c0572fc29159aa037cd695bd4e8ce1cac
Ramzi <48523685+ramziash@users.noreply.github.com>
2019-11-28 15:15:48 +0100

    Update e8-type checker.js

--- Week1/homework/js-excercises/e8-type checker.js
+++ Week1/homework/js-excercises/e8-type checker.js
@@ -1,17 +1,45 @@
+// 'use strict'
+
+// let string1='string1';
+// let string2='string2';
+// let object1=['HYF', 25];
+// let object2=['HYF', 24];
+
+
+// typeof string1==typeof string2
+// typeof string1==typeof object1
+// typeof string1==typeof object2
+// typeof string2==typeof string1
+// typeof string2==typeof object1
+// typeof string2==typeof object2
+
+
+// console.log(typeof string1 && typeof string2)
+// console.log(typeof object1 && typeof object2)
+
+// if (typeof string1 == typeof object1){
+//     return console.log('Same type')
+// } else{
+//     return console.log('Different type')
+// }
+
+
+
 'use strict'
 
 let string1='string1';
 let string2='string2';
-let object1=['HYF', 25];
-let object2=['HYF', 24];
+let object1={position: 'student'};
+let object2={available: true};
 
 
-typeof string1==typeof string2
-typeof string1==typeof object1
-typeof string1==typeof object2
-typeof string2==typeof string1
-typeof string2==typeof object1
-typeof string2==typeof object2
+console.log(typeof string1==typeof string2)
+console.log(typeof string1==typeof object1)
+console.log(typeof string1==typeof object2)
+console.log(typeof string2==typeof string1)
+console.log(typeof string2==typeof object1)
+console.log(typeof string2==typeof object2)
+
 
 
 console.log(typeof string1 && typeof string2)
@@ -22,3 +50,5 @@ if (typeof string1 == typeof object1){
 } else{
     return console.log('Different type')
 }
+
+

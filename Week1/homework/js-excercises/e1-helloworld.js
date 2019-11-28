9118e3bcf71bc02d31dd411b9b2783d4ef50c98f
Ramzi <48523685+ramziash@users.noreply.github.com>
2019-11-28 14:51:56 +0100

    Update e1-helloworld.js

--- Week1/homework/js-excercises/e1-helloworld.js
+++ Week1/homework/js-excercises/e1-helloworld.js
@@ -5,3 +5,14 @@ const hello= ['hello world', 'halo Dunia', 'hola mundo', 'Bonjour le monde',
             'hej varlden', 'مرحبا بالعالم'];
 
 console.log(hello);
+
+console.log('hello world')
+console.log('halo Dunia')
+console.log('halo mundo')
+console.log('bonsjour le monde')
+console.log('ハロー・ワールド')
+console.log('hallo wereld')
+console.log('ciao mondo')
+console.log('witaj swecie')
+console.log('hej varlden')
+console.log('مرحبا بالعالم')

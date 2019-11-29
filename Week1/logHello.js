3398bb518f3e6c6db2b693f7fd9d606bd49f9c5f
Enwer <52347489+erdem524@users.noreply.github.com>
2019-11-29 10:26:10 +0100

    Create logHello.js

---
+++ Week1/logHello.js
@@ -0,0 +1,26 @@
+'use strict'
+var a = 'Hello'+ " " +'world!';
+console.log(a); 
+var a = 'Selam ' + " " +'Dünya';
+console.log(a); 
+var a = 'salve ' + " " +'mundi!';
+console.log(a); 
+var a = 'こんにちは' + " " +'世界!';
+console.log(a); 
+var a = '안녕 ' + " " +'세상!';
+console.log(a); 
+var a = 'Hallo'+ " " +'wereld!';
+console.log(a); 
+var a = 'مرحبا'+ " " +'عالم!';
+console.log(a); 
+var a = 'سلام' + " " +'دنیا!';
+console.log(a); 
+var a = 'سالام'+ " " +'دۇنيا!';
+console.log(a); 
+var a = 'ciao'+ " " +'mondo!';
+console.log(a); 
+
+
+
+
+

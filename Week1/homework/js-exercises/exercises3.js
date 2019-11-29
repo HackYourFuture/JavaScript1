e6259e5ba1da43aa860474d6bb9691f008a37e1c
AmnaAhmad <moni0designer@gmail.com>
2019-11-29 07:17:16 +0100

    my home work

---
+++ Week1/homework/js-exercises/exercises3.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let numberX;
+console.log('undefined, because the variable is not assigned a value');
+console.log(numberX);
+numberX = 10;
+console.log('10, because now it has been assigned a value 10');
+console.log(numberX);
---
+++ Week1/homework/js-exercises/logHallo.js
@@ -0,0 +1,11 @@
+'use strict';
+console.log('Hello ' + 'World');        // English
+console.log('Halo ' + 'Mundo');         //Spanish 
+console.log('こんにちは ' + '世界');      //日本人 
+console.log('Bongu ' + 'Tad-dinja');    // Maltese
+console.log('שלום ' + 'עולם');           // Hebrew 
+console.log('مرحبا ' + 'العالمية');           // Arabic
+console.log('Hej ' + 'Verden');           // Danish
+console.log('алло ' + 'Мир');           // Russian
+console.log('هالو ' + 'ودرلد');           // Persian
+console.log('slav ' + 'Dinya');           // Kurdish

1890591fb1ec01b740c2adddef9a30edf5b07ad3
Ramzi <48523685+ramziash@users.noreply.github.com>
2019-11-30 11:01:40 +0100

    Update e8-type checker.js

--- Week1/homework/js-excercises/e8-type checker.js
+++ Week1/homework/js-excercises/e8-type checker.js
@@ -1,29 +1,3 @@
-// 'use strict'
-
-// let string1='string1';
-// let string2='string2';
-// let object1=['HYF', 25];
-// let object2=['HYF', 24];
-
-
-// typeof string1==typeof string2
-// typeof string1==typeof object1
-// typeof string1==typeof object2
-// typeof string2==typeof string1
-// typeof string2==typeof object1
-// typeof string2==typeof object2
-
-
-// console.log(typeof string1 && typeof string2)
-// console.log(typeof object1 && typeof object2)
-
-// if (typeof string1 == typeof object1){
-//     return console.log('Same type')
-// } else{
-//     return console.log('Different type')
-// }
-
-
 
 'use strict'
 

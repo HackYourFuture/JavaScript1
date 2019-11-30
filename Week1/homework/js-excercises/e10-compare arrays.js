14e1fc0be0a70e1454d2e9904a3aac062097149f
Ramzi <48523685+ramziash@users.noreply.github.com>
2019-11-30 11:03:26 +0100

    Update e10-compare arrays.js

--- Week1/homework/js-excercises/e10-compare arrays.js
+++ Week1/homework/js-excercises/e10-compare arrays.js
@@ -1,27 +1,13 @@
-// 'use strict'
-
-// const myArray1=['HYF', 25, '#bestclassever', {instructor: 'Wilgert'}]
-// const myArray2=['study', 'read', 'hangout', 'eat', 'drink', 'live', 101]
-
-// console.log(myArray1.length)
-// console.log(myArray2.length)
-
-// if (myArray1.length==myArray2.length) {
-//     console.log('Same size!')
-// } else{
-//     console.log('Two different sizes')
-// }
-
 
 'use strict'
 
-const JS_class25=['HYF', 25, '#bestclassever', {instructor: 'Wilgert'}]
-const Hobbies=['study', 'read', 'hangout', 'eat', 'drink', 'live', 101]
+const jsClass25=['HYF', 25, '#bestclassever', {instructor: 'Wilgert'}]
+const hobbies=['study', 'read', 'hangout', 'eat', 'drink', 'live', 101]
 
-console.log(JS_class25.length)
-console.log(Hobbies.length)
+console.log(jsClass25.length)
+console.log(hobbies.length)
 
-if (JS_class25.length==Hobbies.length) {
+if (jsClass25.length==hobbies.length) {
     console.log('Same size!')
 } else{
     console.log('Two different sizes')

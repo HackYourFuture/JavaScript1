95d8970db5797dee71e3f78b53780e0585eda3c4
hanbingli <57145537+hanbingli@users.noreply.github.com>
2019-12-01 15:41:07 +0100

    problem fixed

--- Week1/homework/Exercise4.js
+++ Week1/homework/Exercise4.js
@@ -5,6 +5,6 @@
 let myString = 'Hanbing Li';
 console.log('its value should be \"Hanbing Li\"');
 console.log(myString);
-myString = "it's a new string";
-console.log("its value should be 'it's a new string'");
+myString = ‘it's a new string’;
+console.log('its value should be it\'s a new string\'');
 console.log(myString);
--- Week1/homework/Exercise6.js
+++ Week1/homework/Exercise6.js
@@ -3,7 +3,7 @@
 'use strict';
 
 let array = [];
-console.log('Maybe it should be \"undefined\"?');
+console.log('it\'s an empty array);
 console.log(array);
 let favoriteAnimals = ['cats', 'dogs', 'penguins'];
 console.log(favoriteAnimals);

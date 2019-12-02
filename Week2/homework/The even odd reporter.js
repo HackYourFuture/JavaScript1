c5b5545086f8192bb05121ab242e74037e688798
Ammar <zarosho@gmail.com>
2019-12-02 20:27:13 +0100

    ex2 is done

---
+++ Week2/homework/The even odd reporter.js
@@ -0,0 +1,20 @@
+'use strict';
+
+ for (let i = 0; i <= 20; i++){
+ console.log(i);
+}
+
+for (let i =0; i <= 20 ; i++ ) {
+    if (i % 2 === 0) { 
+    console.log('The number ' + i + ' is even!');
+    } else { 
+        console.log('The number ' + i + ' is odd!')
+    }
+}
+
+
+for (let i =0; i <= 20 ; i++ ) {
+    if (i % 2 === 1) { 
+    console.log('The number ' + i + ' is odd!')
+    }
+}

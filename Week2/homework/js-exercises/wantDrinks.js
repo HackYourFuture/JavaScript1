59719006ffc0f4e509daf38226b200b736fd0165
Enwer <52347489+erdem524@users.noreply.github.com>
2019-12-05 20:43:07 +0100

    wantDrinks.js

--- Week2/homework/js-exercises/wantDrinks.js
+++ Week2/homework/js-exercises/wantDrinks.js
@@ -10,8 +10,3 @@ for (let i = 0; i <5; i++){
 // 5. Log to the console: "Hey guys, I brought a [INSERT VALUES FROM ARRAY]!"
 console.log (`Hey guys, I brought a ${drinkTray.join()}`);
 
-
-
-// 3. If there are already two instances of a`drinkType` then start with the next drink in the array.
-// 4. Your`drinkTray` should contain 2 cola, 2 lemonade and 1 water.
-// (For example: "Hey guys, I brought a cola, cola, lemonade, lemonade, water!")

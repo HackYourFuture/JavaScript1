def06bfc38090bc4524e6ab1e6dedd1515288f5f
Naji <najin546@gmail.com>
2019-12-16 10:04:14 +0100

    homework repaired

--- Week3/week3HomeWork/Exercise3/fortuneTeller.js
+++ Week3/week3HomeWork/Exercise3/fortuneTeller.js
@@ -1,14 +1,26 @@
 const numChildren = [1, 2, 3, 4, 5];
-const partnerNames = ['Jessica', 'Wilma', 'Nadia', 'Susan', 'Angelina'];
-const locations = ['Syria', 'London', 'Turkey', 'Netherland', 'Franca'];
-const jobs = ['accountant', 'brogrammer', 'salesman', 'businessman', 'manager'];
+const partnerNames = ["Jessica", "Wilma", "Nadia", "Susan", "Angelina"];
+const locations = ["Syria", "London", "Turkey", "Netherland", "Franca"];
+const jobs = ["accountant", "brogrammer", "salesman", "businessman", "manager"];
 
 function tellFortune(child, partener, location, job) {
+    const numChildren = [1, 2, 3, 4, 5];
+    const partnerNames = ["Jessica", "Wilma", "Nadia", "Susan", "Angelina"];
+    const locations = ["Syria", "London", "Turkey", "Netherland", "Franca"];
+    const jobs = [
+        "accountant",
+        "brogrammer",
+        "salesman",
+        "businessman",
+        "manager"
+    ];
     child = numChildren[Math.floor(Math.random() * 5)];
     partener = partnerNames[Math.floor(Math.random() * 5)];
     location = locations[Math.floor(Math.random() * 5)];
     job = jobs[Math.floor(Math.random() * 5)];
-    console.log(`You will be a ${job} in ${location}, and married to ${partener} with ${child} Kids`)
+    console.log(
+        `You will be a ${job} in ${location}, and married to ${partener} with ${child} Kids`
+    );
 }
 
 tellFortune();
@@ -19,7 +31,9 @@ function newTellFortune(child, partener, location, job) {
     let parteners = partnerNames[Math.floor(Math.random() * partener)];
     let numLocationes = locations[Math.floor(Math.random() * location)];
     let numJobs = jobs[Math.floor(Math.random() * job)];
-    console.log(`You will be a ${numJobs} in ${numLocationes}, and married to ${parteners} with ${childern} Kids`)
+    console.log(
+        `You will be a ${numJobs} in ${numLocationes}, and married to ${parteners} with ${childern} Kids`
+    );
 }
 
 newTellFortune(5, 5, 3, 4);

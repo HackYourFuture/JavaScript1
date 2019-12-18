0bb4b9790c07ed106a622bb9bea8f604e0cb3443
Ramzi <48523685+ramziash@users.noreply.github.com>
2019-12-18 09:47:20 +0100

    Update e1-youAreAmazing.js

--- Week3/homework/js-exercises/e1-youAreAmazing.js
+++ Week3/homework/js-exercises/e1-youAreAmazing.js
@@ -2,8 +2,6 @@
 
 function giveCompliment (name) {
 
-    name=name.charAt(0).toUpperCase() + name.slice(1);
-
     const complimentList = ['great', 'awesome', 'kind', 'reliable','witty', 'optimistic','patient','outgoing','energetic','artistic']
 
     let compliment = complimentList[Math.floor(Math.random()*complimentList.length)];

998892f6372ee6ccb489bf7cc5364274e5e3945a
Mucahit <mucahit04@gmail.com>
2020-01-21 22:45:57 +0100

    exercise logging remainder is done

---
+++ Week1/homework/js-exercises/log_remainder.js
@@ -0,0 +1,25 @@
+'use strict';
+/*
+1- If x equals 7, and the only other statement is x = x % 3, what would be the value of x 
+after the calculation ?*/
+
+let x = 7; // this is the decleration and assigning the value of 7 to x.
+x = x % 3; /* This piece of code assigns a new value to the x and that is the remainder of x/3. The remainder of 7/3= 1 */
+console.log(x);
+
+/*
+2- If y equals 21, and the only other statement is y = y % 4, what would be the value of y
+after the calculation ?*/
+
+let y = 21; // this is the decleration and assigning the value of 21 to y.
+y = y % 4; /* This piece of code assigns a new value to the y and that is the remainder of y/4. The remainder of 21/4= 1 */
+console.log(y);
+
+
+/*
+3- If z equals 13, and the only other statement is z = z % 2, what would be the value of z
+//after the calculation ?*/
+
+let z = 13; // this is the decleration and assigning the value of 13 to z.
+z = z % 2; /* This piece of code assigns a new value to the z and that is the remainder of z/2. The remainder of 13/2= 1 */
+console.log(z);

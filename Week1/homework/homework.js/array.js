c4c92925f595f48d93fd3b2427da82cf7777d584
Huseyin T <buenasdias1071@gmail.com>
2020-01-24 23:03:57 +0100

    js week1 homework

---
+++ Week1/homework/homework.js/array.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let animals = [];
+console.log('the value of animals is null');
+console.log(animals);
+let myFavoriteAnimals = ['eagle', 'wolf', 'tiger'];
+console.log(myFavoriteAnimals);
+myFavoriteAnimals[myFavoriteAnimals.length] = 'piglet';
+console.log(myFavoriteAnimals);

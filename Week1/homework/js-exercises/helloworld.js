1f552bc27000adc282470619115398ee6e2e66b4
ahmed shahwan <shahwanahmed15@outlook.com>
2020-03-18 19:00:15 +0100

    first msg

---
+++ Week1/homework/js-exercises/helloworld.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('Hello world!'); //English
+console.log('Hallo Wereld!'); //Dutch
+console.log('Halo, dunia!'); //Indonesian
+console.log('Ciao, mondo!'); //Italian
+console.log('Hola, mundo!'); //Spanish
+console.log('Bonjour le monde'); //France
+console.log('Привет, мир'); //russian
+console.log('こんにちは世界'); //Japanese
+console.log('مرحبا'); //Arabic
+console.log('नमस्ते दुनिया'); //Hindi

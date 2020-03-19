f5420035bcc71023b9213f055f55409e93768fa9
DoganOnder <61209132+DoganOnder@users.noreply.github.com>
2020-03-19 13:28:28 +0100

    Update logHello.js

--- Week1/homework/js-exercises/logHello.js
+++ Week1/homework/js-exercises/logHello.js
@@ -1,1 +1,11 @@
-
+"use strict";
+console.log("Merhaba Dunya!");  //Turkish
+console.log("Hallo Wereld!"); //Dutch
+console.log("Hello World!");  //English
+console.log ("Ciao Mundo!"); //Italian
+console.log ("Hola Mundo!"); // Spanish
+console.log("Hallo Dunia!");  //Indonesian
+console.log ("Hallo Welt!"): //German
+console.log ("Olá Mundo!"); //Portuguese
+console.log ("مرحبا بالعالم"); //Arabic
+console.log ("Hej världen!"); //Swedish

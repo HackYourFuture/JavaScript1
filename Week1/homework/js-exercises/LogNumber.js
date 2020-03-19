ff64605bf3584bf1dc862a683047998ee80f373f
DoganOnder <61209132+DoganOnder@users.noreply.github.com>
2020-03-19 13:46:48 +0100

    Create LogNumber.js

---
+++ Week1/homework/js-exercises/LogNumber.js
@@ -0,0 +1,8 @@
+"use strict";
+let numberX;
+console.log("The value of numberX is undefined");
+
+console.log(numberX);
+numberX=66;
+console.log(The value of numberX is 66");
+console.log(numberX);

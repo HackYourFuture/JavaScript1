6836504384e7f49caea4a58edca9055c05c3f2d0
DoganOnder <61209132+DoganOnder@users.noreply.github.com>
2020-03-20 10:09:02 +0100

    Create arrayAnimals.js

---
+++ Week1/homework/js-exercises/arrayAnimals.js
@@ -0,0 +1,8 @@
+"use strict";
+let items =[];
+console.log("this is an empty array"];
+console.log(items);
+let animals=["bird","fish", "cat"];
+console.log(animals);
+animals.push("Piglets");
+console.log(animals);

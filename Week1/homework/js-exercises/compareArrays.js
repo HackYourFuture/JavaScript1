3e5202a63fd290d5529a16f0e5ef5870d4b6b181
DoganOnder <61209132+DoganOnder@users.noreply.github.com>
2020-03-20 11:46:48 +0100

    Create compareArrays.js

---
+++ Week1/homework/js-exercises/compareArrays.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const arr1 =["Hello", 123, true, {name:"Dogan"}];
+
+const arr2=[99, true, "Dogan", king, null, {name:"Dogan"}, "London"];
+
+console.log(arr1.length);
+console.log(arr2.length);
+
+if (arr1.length === arr2.length) {console.log("They are the same!);}
+else {console.log("two different sizes");}

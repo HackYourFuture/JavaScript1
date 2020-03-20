efc4e61bf635fcd37c66c2b30e755e50d1b2fbec
DoganOnder <61209132+DoganOnder@users.noreply.github.com>
2020-03-20 09:36:03 +0100

    Create logString.js

---
+++ Week1/homework/js-exercises/logString.js
@@ -0,0 +1,7 @@
+"use strict";
+let myString = "Dogan Onder";
+console.log ("The value of myString is 'Dogan Onder'");
+console.log(myString);
+myString="Ozgur Dogan";
+console.log("The value of myString is 'Ozgur Dogan' now");
+console.log(myString);

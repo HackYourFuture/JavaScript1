a6dcca10ee271831139a470d4f6c56101a29d363
Omer Degirmenci <rumidegirmenci0@gmail.com>
2020-03-21 22:46:58 +0100

    check exercises

--- Week1/homework/6-log-an-an-array-of-animals.js
+++ Week1/homework/6-log-an-an-array-of-animals.js
@@ -1,7 +1,20 @@
+//Declare variable and assign to it an empty array. Make sure that the name of the variable indicates it contains more than 1 item. For example items instead of item.
 let friends = [];
-console.log('friend1, friend2, friend3');
+
+//Write a console.log statement that explains in words what you think the value of the array is.
+console.log("the value of the array is an empty array")
+
+//Write a console.log statement that logs the array.
 console.log(friends);
+
+//Create a new variable with an array that has 3 of your favorite animals, each in a different string. Make sure the name of the variables says something about what the variable contains.
 let favAnimals = ["cats", "birds", "sheeps"];
+
+//Write a console.log statement that logs the second array.
 console.log(favAnimals);
+
+//Add a statement that adds another string ("Piglet)" to the array of animals.
 favAnimals[3] = "Piglet";
+
+//Write a console.log statement that logs the second array!
 console.log(favAnimals);

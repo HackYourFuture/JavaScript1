2f3a7a83977cd11324da4ad172eaa308f0b7dcb0
Ömer <omerbkk06@gmail.com>
2020-03-26 12:56:33 +0100

    Wrong upload

--- Week2/homework/js-exercies/1-removeComma.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-let myString = "hello,this,is,a,difficult,to,read,sentence";
-
-console.log(myString.length);
-
-const regex = /,/g;
-myString = myString.replace(regex, ' ');
-
-console.log(myString);

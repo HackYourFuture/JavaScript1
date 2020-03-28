f3cf07bcb2347b9f8324950db29d1ff60f44edfa
Baraa Kolly <baraa.ali.kolly@gmail.com>
2020-03-28 18:45:48 +0100

    update drinkList update

--- Week2/homework/js-exercises/drinkList.js
+++ Week2/homework/js-exercises/drinkList.js
@@ -1,20 +1,35 @@
 "use strict";
 let drinkTray = [];
+
 const drinkTypes = ["cola", "lemonade", "water"];
+function myShuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+}
+return array;
+}
 
 function fillTray(tray) {
+    let shuffleDrinkTypes = myShuffle(drinkTypes);
+    let drink;
+    let drinkObjList = [];
+  for( drink of shuffleDrinkTypes){
+     let myObject = {name:drink , counter:0};
+     drinkObjList.push(myObject);
 
-    let colaCounter = 0;
-    let lemonadeCounter = 0;
-    let waterCounter = 0;
-    let i=0;
-    while (i<5){
-      if (colaCounter < 2) { tray.push(drinkTypes[0]); i++; colaCounter++; }
-      else
-      if (lemonadeCounter < 2)  { tray.push(drinkTypes[1]); i++; lemonadeCounter++; } 
-      else
-      if (waterCounter < 2)  { tray.push(drinkTypes[2]); i++; waterCounter++; }
-    }
+  }
+    let i = 0;
+        for(drink of drinkObjList){
+            while(i<5 && drink.counter<2) { 
+                tray.push(drink.name); 
+                drink.counter++; 
+                i++; 
+           }
+        }
+        
     return tray;
 }
 console.log(`Hey guys, I brought a ${fillTray(drinkTray)}`);

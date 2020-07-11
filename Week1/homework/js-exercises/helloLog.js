711442c4f0c3bfd3a74749848ef44d3d1555a99c
ismaiel <ismaiel.nab@gmail.com>
2020-07-11 15:28:59 +0200

    create helloLog.js 1st exercise

---
+++ Week1/homework/js-exercises/helloLog.js
@@ -0,0 +1,22 @@
+var greetingWorld = "Hello World! 'English'"; // English
+console.log(greetingWorld);
+greetingWorld = "Hallo Wereld! // Dutch"; // Dutch
+console.log(greetingWorld);
+greetingWorld = "مرحبا بالعالم! // Arabic"; // Arabic
+console.log(greetingWorld);
+greetingWorld = "こんにちは世界！// Japanese"; // Japanese
+console.log(greetingWorld);
+greetingWorld = "Hallo Welt!  //  German"; // German
+console.log(greetingWorld);
+greetingWorld = "Bonjour le monde! //  French"; // French
+console.log(greetingWorld);
+greetingWorld = "नमस्ते दुनिया!  //  Hindi"; // Hindi
+console.log(greetingWorld);
+greetingWorld = "Hello World!  //  Filipino"; // Filipino
+console.log(greetingWorld);
+greetingWorld = "안녕하세요 세계! //  Korian"; // Korian
+console.log(greetingWorld);
+greetingWorld = "Hej Verden!  //  Dinish"; // Dinish
+console.log(greetingWorld);
+greetingWorld = "Sawubona Mhlaba!  //  Zulu"; // Zulu
+console.log(greetingWorld);

3ae2e8a0278aa2129d1690bb81de3d8d75603c1e
Vaida <93vaida@gmail.com>
2020-09-02 17:47:31 +0200

    wrote a hello world log in 10 languages

---
+++ Week1/Homework/js-exercises/helloLog.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log("Hello World!"); // English
+console.log("Hallo Wereld!"); // Dutch
+console.log("Labas pasauli!"); // Lithuanian
+console.log("Bonjour le monde!"); // French
+console.log("Hola Mundo!"); // Spanish
+console.log("Konnichiwa sekai"); // Japanese
+console.log("Zdravo Svete!"); // Serbian
+console.log("Hej Verden!"); // Danish
+console.log("Orbis Terrarum salve!"); // Latin
+console.log("Saluton mondo!"); // Esperanto

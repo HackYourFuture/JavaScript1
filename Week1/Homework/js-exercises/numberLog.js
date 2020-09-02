50ea4336a4cd0245c16c416e82fa080f80d17a67
Vaida <93vaida@gmail.com>
2020-09-02 19:36:15 +0200

    created numberX variable

---
+++ Week1/Homework/js-exercises/numberLog.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var numberX;
+console.log("Value of X can be any given number");
+console.log(numberX); //undefined
+var numberX = 9;
+console.log("Value of numberX now is 9");
+console.log(numberX); //node prints out 9

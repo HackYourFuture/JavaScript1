3ebd3f58ff8cffdf8c3db309d1524749137ba419
Vaida <93vaida@gmail.com>
2020-09-02 20:11:53 +0200

    created a variable that contains list of animals

---
+++ Week1/Homework/js-exercises/arrayAnimals.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var myArrays = [];
+console.log("The value of the array is empty, null. There is nothing in that array");
+console.log(myAnimals);
+var myAnimals = ["Cats", "Dogs", "Parakeets"];
+console.log(myAnimals);
+myAnimals.push("Piglet");
+console.log(myAnimals);
+
+

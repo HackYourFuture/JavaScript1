3303085ca54d8c253bac595470bff4ce1686a735
Vaida <93vaida@gmail.com>
2020-09-02 19:41:02 +0200

    create myString variable and gave it two different strings

---
+++ Week1/Homework/js-exercises/stringLog.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var myString = "Vaida Vaitkeviciute";
+console.log("value of myString is set to my first and last name");
+console.log(myString);
+var myString = "Totally Different Person";
+console.log("Now myString will show a Totally Different Person");
+console.log(myString);

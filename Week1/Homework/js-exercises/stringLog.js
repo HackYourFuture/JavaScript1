8f1b0cc904c6095bb373b0d61ba1b9f8134f8a84
Vaida <93vaida@gmail.com>
2020-09-06 15:16:44 +0200

    changed keyword var to let

--- Week1/Homework/js-exercises/stringLog.js
+++ Week1/Homework/js-exercises/stringLog.js
@@ -1,8 +1,7 @@
 "use strict";
-//using var instead of let, as let does not allow to change the value of variable
-var myString = "Vaida Vaitkeviciute";
+let myString = "Vaida Vaitkeviciute";
 console.log("value of myString is set to my first and last name");
 console.log(myString);
-var myString = "Totally Different Person";
+myString = "Totally Different Person";
 console.log("Now myString will show a Totally Different Person");
 console.log(myString);

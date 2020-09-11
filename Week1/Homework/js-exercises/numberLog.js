53a8b14ec9ec2bd5dd3b00419f89909753a545c4
Vaida <93vaida@gmail.com>
2020-09-11 20:10:20 +0200

    added coment to make it more clear what was the task

--- Week1/Homework/js-exercises/numberLog.js
+++ Week1/Homework/js-exercises/numberLog.js
@@ -1,7 +1,10 @@
+//declare variable numberX without a value.  explain in words what you think the value of numberX. Log value of numberX. Initialize variable with any number. Add console log to explain what you think the value going to be. Add a console log that shows the value of numberX
+
+
 "use strict";
 
 let numberX;
-console.log("Value of X can be any given number");
+console.log("Value of numberX it`s not defined. It can be anything, but name suggests that we should assign it value of a number.");
 console.log(numberX); //undefined
 numberX = 9;
 console.log("Value of numberX now is 9");
